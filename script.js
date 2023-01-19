7ee37c1396b25b0892889f928c5b25ce69c98a01
jose-the-lima <joserennyer30@gmail.com>
2023-01-19 11:00:40 -0300

    adicionado o javascript no projeto e importando a biblioteca da rocketseat nlwsetup

---
+++ script.js
@@ -0,0 +1,13 @@
+const form = document.querySelector("#habits-form")
+
+const nlwSetup = new NLWSetup(form)
+
+let data = {
+  run: ["01-17", "01-18", "01-19"],
+  food: ["01-15", "01-16", "01-17"],
+  water: ["01-15", "01-16", "01-17", "01-18", "01-19"],
+  study: ["01-15", "01-16", "01-17", "01-18", "01-19", "01-20"],
+}
+
+nlwSetup.setData(data)
+nlwSetup.load()
