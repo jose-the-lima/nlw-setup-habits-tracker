36efb0a0a7e1816deba230b824a16ffbdce09b93
jose-the-lima <joserennyer30@gmail.com>
2023-01-20 11:03:56 -0300

    adicionado logica para adição e save da data, adicionado também conexao com o localStorage

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 const form = document.querySelector("#habits-form")
-
 const nlwSetup = new NLWSetup(form)
+const buttonAdd = document.querySelector(".button-add")
 
-let data = {
-  run: ["01-17", "01-18", "01-19"],
-  food: ["01-15", "01-16", "01-17"],
-  water: ["01-15", "01-16", "01-17", "01-18", "01-19"],
-  study: ["01-15", "01-16", "01-17", "01-18", "01-19"],
+buttonAdd.addEventListener("click", add)
+
+// Agora vamos adicionar um evento ao "formulário", ele vai simplesmente ativar a função "save" a cada vez que o formulário for alterado, e o nome do evento para alterações no formulário é "change"
+
+form.addEventListener("change", save)
+
+function add() {
+  const dataAtual = new Date().toLocaleDateString("pt-br").slice(0, 5)
+  const existeEssaData = nlwSetup.dayExists(dataAtual)
+
+  if (existeEssaData) {
+    alert("A data já está inclusa 🚫")
+    return
+  }
+
+  nlwSetup.addDay(dataAtual)
 }
 
-nlwSetup.setData(data)
+function save() {
+  let dados = nlwSetup.data
+
+  // E a cada vez que o save for utilizado, ele vai pegar os dados atuais e vai salvar no localStorage com o método setItem
+
+  localStorage.setItem("habitos", JSON.stringify(dados))
+
+  // A cada vez que o formulário for alterado, então será chamado essa função que vai salvar os dados atuais em um localStorage com o formato de string.
+}
+
+// Para sempre que o meu programa for aberto, ele chamar todos os dados que já foram salvos ultimamente, eu vou precisar pegar os dados salvos, correto ? Então para isso eu vou utilizar o getItem e passar a chave onde se encontra os dados
+
+const dadosAtuais = JSON.parse(localStorage.getItem("habitos")) || {}
+
+nlwSetup.setData(dadosAtuais)
 nlwSetup.load()
