0ff14b8cb1eb0e6d4349eda65a53dab35ab5218c
jose-the-lima <joserennyer30@gmail.com>
2023-01-21 08:09:29 -0300

    removed the comments and added a new habit

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const buttonAdd = document.querySelector(".button-add")
 
 buttonAdd.addEventListener("click", add)
 
-// Agora vamos adicionar um evento ao "formulário", ele vai simplesmente ativar a função "save" a cada vez que o formulário for alterado, e o nome do evento para alterações no formulário é "change"
-
 form.addEventListener("change", save)
 
 function add() {
@@ -23,14 +21,8 @@ function add() {
 function save() {
   let dados = nlwSetup.data
 
-  // E a cada vez que o save for utilizado, ele vai pegar os dados atuais e vai salvar no localStorage com o método setItem
-
   localStorage.setItem("habitos", JSON.stringify(dados))
-
-  // A cada vez que o formulário for alterado, então será chamado essa função que vai salvar os dados atuais em um localStorage com o formato de string.
 }
-
-// Para sempre que o meu programa for aberto, ele chamar todos os dados que já foram salvos ultimamente, eu vou precisar pegar os dados salvos, correto ? Então para isso eu vou utilizar o getItem e passar a chave onde se encontra os dados
 
 const dadosAtuais = JSON.parse(localStorage.getItem("habitos")) || {}
 
